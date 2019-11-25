dbaa9bf1e0d8262c4a7ed33a68344096a9ceb7c2
Marcelo1860 <chelobader@gmail.com>
2019-11-25 16:51:40 -0300

    Adaptación interfaz grafica a versión 5.3

--- Programa NovoSens 5.4 GUI/Programa NovoSens 5.4 GUI/Form1.Designer.cs
+++ Programa NovoSens 5.4 GUI/Programa NovoSens 5.4 GUI/Form1.Designer.cs
@@ -28,13 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CantSaltosx = new System.Windows.Forms.CheckBox();
+            this.VectDatosx = new System.Windows.Forms.CheckBox();
+            this.DesvSaltosx = new System.Windows.Forms.CheckBox();
+            this.MediaSaltosx = new System.Windows.Forms.CheckBox();
+            this.Saltosx = new System.Windows.Forms.CheckBox();
+            this.DesvResultx = new System.Windows.Forms.CheckBox();
+            this.MediaResultx = new System.Windows.Forms.CheckBox();
+            this.Resultadosx = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Examinar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(284, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Archivo de datos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Programa_NovoSens_5._4_GUI.Properties.Resources.novo_logo_2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CantSaltosx
+            // 
+            this.CantSaltosx.AutoSize = true;
+            this.CantSaltosx.BackColor = System.Drawing.Color.Teal;
+            this.CantSaltosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CantSaltosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CantSaltosx.Location = new System.Drawing.Point(182, 373);
+            this.CantSaltosx.Name = "CantSaltosx";
+            this.CantSaltosx.Size = new System.Drawing.Size(131, 17);
+            this.CantSaltosx.TabIndex = 31;
+            this.CantSaltosx.Text = "Cantidad de saltos";
+            this.CantSaltosx.UseVisualStyleBackColor = false;
+            // 
+            // VectDatosx
+            // 
+            this.VectDatosx.AutoSize = true;
+            this.VectDatosx.BackColor = System.Drawing.Color.Teal;
+            this.VectDatosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VectDatosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.VectDatosx.Location = new System.Drawing.Point(23, 373);
+            this.VectDatosx.Name = "VectDatosx";
+            this.VectDatosx.Size = new System.Drawing.Size(116, 17);
+            this.VectDatosx.TabIndex = 30;
+            this.VectDatosx.Text = "Vector de datos";
+            this.VectDatosx.UseVisualStyleBackColor = false;
+            // 
+            // DesvSaltosx
+            // 
+            this.DesvSaltosx.AutoSize = true;
+            this.DesvSaltosx.BackColor = System.Drawing.Color.Teal;
+            this.DesvSaltosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesvSaltosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DesvSaltosx.Location = new System.Drawing.Point(182, 302);
+            this.DesvSaltosx.Name = "DesvSaltosx";
+            this.DesvSaltosx.Size = new System.Drawing.Size(151, 17);
+            this.DesvSaltosx.TabIndex = 29;
+            this.DesvSaltosx.Text = "Desv. estandar Saltos";
+            this.DesvSaltosx.UseVisualStyleBackColor = false;
+            // 
+            // MediaSaltosx
+            // 
+            this.MediaSaltosx.AutoSize = true;
+            this.MediaSaltosx.BackColor = System.Drawing.Color.Teal;
+            this.MediaSaltosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MediaSaltosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MediaSaltosx.Location = new System.Drawing.Point(182, 279);
+            this.MediaSaltosx.Name = "MediaSaltosx";
+            this.MediaSaltosx.Size = new System.Drawing.Size(99, 17);
+            this.MediaSaltosx.TabIndex = 28;
+            this.MediaSaltosx.Text = "Media Saltos";
+            this.MediaSaltosx.UseVisualStyleBackColor = false;
+            // 
+            // Saltosx
+            // 
+            this.Saltosx.AutoSize = true;
+            this.Saltosx.BackColor = System.Drawing.Color.Teal;
+            this.Saltosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Saltosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Saltosx.Location = new System.Drawing.Point(182, 256);
+            this.Saltosx.Name = "Saltosx";
+            this.Saltosx.Size = new System.Drawing.Size(61, 17);
+            this.Saltosx.TabIndex = 27;
+            this.Saltosx.Text = "Saltos";
+            this.Saltosx.UseVisualStyleBackColor = false;
+            // 
+            // DesvResultx
+            // 
+            this.DesvResultx.AutoSize = true;
+            this.DesvResultx.BackColor = System.Drawing.Color.Teal;
+            this.DesvResultx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesvResultx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DesvResultx.Location = new System.Drawing.Point(23, 302);
+            this.DesvResultx.Name = "DesvResultx";
+            this.DesvResultx.Size = new System.Drawing.Size(156, 17);
+            this.DesvResultx.TabIndex = 26;
+            this.DesvResultx.Text = "Desv. estandar Result.";
+            this.DesvResultx.UseVisualStyleBackColor = false;
+            // 
+            // MediaResultx
+            // 
+            this.MediaResultx.AutoSize = true;
+            this.MediaResultx.BackColor = System.Drawing.Color.Teal;
+            this.MediaResultx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MediaResultx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MediaResultx.Location = new System.Drawing.Point(23, 279);
+            this.MediaResultx.Name = "MediaResultx";
+            this.MediaResultx.Size = new System.Drawing.Size(104, 17);
+            this.MediaResultx.TabIndex = 25;
+            this.MediaResultx.Text = "Media Result.";
+            this.MediaResultx.UseVisualStyleBackColor = false;
+            // 
+            // Resultadosx
+            // 
+            this.Resultadosx.AutoSize = true;
+            this.Resultadosx.BackColor = System.Drawing.Color.Teal;
+            this.Resultadosx.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Resultadosx.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Resultadosx.Location = new System.Drawing.Point(23, 256);
+            this.Resultadosx.Name = "Resultadosx";
+            this.Resultadosx.Size = new System.Drawing.Size(89, 17);
+            this.Resultadosx.TabIndex = 24;
+            this.Resultadosx.Text = "Resultados";
+            this.Resultadosx.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Etoh 1",
+            "Teq"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 204);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 23;
+            this.comboBox1.Text = "Seleccione equipo";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(350, 204);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(278, 186);
+            this.listBox1.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Programa_NovoSens_5._4_GUI.Properties.Resources.Logo_novo;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Teal;
+            this.button2.Location = new System.Drawing.Point(647, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 124);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Obtener datos";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Examinar
+            // 
+            this.Examinar.BackColor = System.Drawing.Color.White;
+            this.Examinar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Examinar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Examinar.Location = new System.Drawing.Point(647, 79);
+            this.Examinar.Name = "Examinar";
+            this.Examinar.Size = new System.Drawing.Size(130, 47);
+            this.Examinar.TabIndex = 20;
+            this.Examinar.Text = "Examinar";
+            this.Examinar.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CantSaltosx);
+            this.Controls.Add(this.VectDatosx);
+            this.Controls.Add(this.DesvSaltosx);
+            this.Controls.Add(this.MediaSaltosx);
+            this.Controls.Add(this.Saltosx);
+            this.Controls.Add(this.DesvResultx);
+            this.Controls.Add(this.MediaResultx);
+            this.Controls.Add(this.Resultadosx);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Examinar);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox CantSaltosx;
+        private System.Windows.Forms.CheckBox VectDatosx;
+        private System.Windows.Forms.CheckBox DesvSaltosx;
+        private System.Windows.Forms.CheckBox MediaSaltosx;
+        private System.Windows.Forms.CheckBox Saltosx;
+        private System.Windows.Forms.CheckBox DesvResultx;
+        private System.Windows.Forms.CheckBox MediaResultx;
+        private System.Windows.Forms.CheckBox Resultadosx;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Examinar;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
